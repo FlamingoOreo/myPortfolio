e42a9506f769ec768e02b87065dd15848e1806e6
flamingoOreo <davidarbabi19@hotmail.com>
2023-01-19 14:37:29 +0100

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+const fs = require("fs")
+const path = require("path")
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  let data = fs.readFileSync(path.resolve(__dirname, "../data/introductionArray.json"));
+  let recommendationsData = fs.readFileSync(path.resolve(__dirname, "../data/recommendations.json"));
+  let cakes = fs.readFileSync(path.resolve(__dirname, "../data/portfolio.json"));
+  res.render('index', { title: 'Express' , array: JSON.parse(data), data: JSON.parse(recommendationsData), cakes: JSON.parse(cakes)});
+});
+
+module.exports = router;
